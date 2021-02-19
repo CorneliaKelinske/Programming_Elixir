1b2b1c17474e4f61248f098d3c3243b88d9fd87f
Cornelia Kelinske <corneliakelinske@gmail.com>
2021-02-19 14:04:36 -0800

    solved the next exercise with the combination of fizz_buzz and rem

--- fizz_buzz_rem.exs
+++ fizz_buzz_rem.exs
@@ -1,10 +1,14 @@
 fizz_buzz = fn
-  0, _, _ -> IO.puts "Fizz"
-  _, 0, _ -> IO.puts "Buzz"
-  0,0, _ -> IO.puts "FizzBuzz"
-  _, _, a -> IO.puts "#{a}"
+  0, 0, _ -> "FizzBuzz"
+  0, _, _ -> "Fizz"
+  _, 0, _ -> "Buzz"
+  _, _, a -> "#{a}"
 end
 
-fizz_rem = fn n -> IO.puts "#{fizz_buzz.(rem(n,3), rem(n,5), n)}, n" end
+fizz_rem = fn n -> IO.puts "#{fizz_buzz.(rem(n,3), rem(n,5), n)}" end
 
 fizz_rem.(11)
+fizz_rem.(10)
+fizz_rem.(15)
+fizz_rem.(10)
+fizz_rem.(12)
