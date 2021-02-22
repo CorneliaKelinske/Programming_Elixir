756f7db53677aadaf1d14178014ba3b2934bff7c
Cornelia Kelinske <corneliakelinske@gmail.com>
2021-02-21 17:24:19 -0800

    solved the last exercise

--- chop.exs
+++ chop.exs
@@ -1,10 +1,19 @@
 defmodule Chop do
 
-  def guess(a, a..b), do: "#{a} is the correct guess!"
-  def guess(b, a..b), do: "#{b} is the correct guess!"
-  def guess(num, a..b) when num <= div(b,2) do
-    "Is it #{div(b,2)}"
-     Chop.guess(num, a..div(b,2))
+  def guess(a, a..b), do: IO.puts "#{a} is the correct guess!"
+  def guess(b, a..b), do: IO.puts "#{b} is the correct guess!"
+
+  def guess(num, a..b) when num <= div(a+b,2) do
+    {num, a..b} |> IO.inspect(label: "GUESS 3")
+     IO.puts "Is it #{div(a+b,2)}"
+     guess(num, a..div(a+b,2))
+  end
+
+  def guess(num, a..b) when num > div(a+b,2) do
+    {num, a..b} |> IO.inspect(label: "GUESS 4")
+
+    IO.puts "Is it #{b+div(b,2)}, option 2"
+    guess(num, div(a+b,2)..b)
   end
 
 end
