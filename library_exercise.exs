161ba4cdb51ced0dc2f186a4379ccc722d2098b7
Cornelia Kelinske <corneliakelinske@gmail.com>
2021-02-22 11:24:23 -0800

    added some more functions to the library exercise

--- library_exercise.exs
+++ library_exercise.exs
@@ -2,4 +2,8 @@ defmodule Libraries do
 
   def to_string(num), do: :erlang.float_to_binary(num, [decimals: 2])
 
+  def get_op_var(var), do: System.get_env(var)
+
+  def get_ext(filename), do: Path.extname(filename)
+
 end
