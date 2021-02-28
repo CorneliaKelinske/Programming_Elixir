31c8b5cd32ed51476e5ba04f18d825891032782f
Cornelia Kelinske <corneliakelinske@gmail.com>
2021-02-28 06:38:20 -0800

    learning how to work with more complex list patterns

---
+++ weather.exs
@@ -0,0 +1,12 @@
+defmodule WeatherHistory do
+
+
+# getting weather information for location number 27
+
+def for_location_27([]), do: []
+def for_location_27([time, 27, temp, rain | tail]) do
+  [ [time, 27, temp, rain] | for_location_27(tail)]
+end
+def for_location_27([_ | tail]), do: for_location_27(tail)
+
+end
