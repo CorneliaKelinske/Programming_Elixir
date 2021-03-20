6d2c183dfcec465e57821ef96bb013ec21318172
Cornelia Kelinske <corneliakelinske@gmail.com>
2021-03-20 07:56:16 -0700

    added bad guess and game lost logic plus tests

--- hangman/lib/hangman/game.ex
+++ hangman/lib/hangman/game.ex
@@ -1,29 +1,28 @@
 defmodule Hangman.Game do
-
   defstruct(
     turns_left: 7,
     game_state: :initializing,
     letters: [],
-    used: MapSet.new(),
+    used: MapSet.new()
   )
 
   def new_game(word) do
     %Hangman.Game{
-      letters: word |> String.codepoints
+      letters: word |> String.codepoints()
     }
   end
+
   def new_game() do
-    new_game(Dictionary.random_word)
+    new_game(Dictionary.random_word())
   end
 
-  def make_move(game = %{ game_state: state}, _guess) when state in [:won, :lost] do
-    { game, tally(game) }
+  def make_move(game = %{game_state: state}, _guess) when state in [:won, :lost] do
+    {game, tally(game)}
   end
 
   def make_move(game, guess) do
     game = accept_move(game, guess, MapSet.member?(game.used, guess))
-    { game, tally(game)}
-
+    {game, tally(game)}
   end
 
   def accept_move(game, guess, _already_guessed = true) do
@@ -36,15 +35,23 @@ defmodule Hangman.Game do
   end
 
   def score_guess(game, _good_guess = true) do
-    new_state = MapSet.new(game.letters)
-    |> MapSet.subset?(game.used)
-    |> maybe_won
-    Map.put(game, :game_state, new_state)
+    new_state =
+      MapSet.new(game.letters)
+      |> MapSet.subset?(game.used)
+      |> maybe_won
 
+    Map.put(game, :game_state, new_state)
   end
 
-  def score_guess(game, _not_good_guess) do
-    game
+  def score_guess(game = %{ turns_left: 1}, _not_good_guess) do
+    Map.put(game, :game_state, :lost)
+  end
+
+  def score_guess(game = %{ turns_left: turns_left}, _not_good_guess) do
+    %{ game |
+       game_state: :bad_guess,
+       turns_left: turns_left - 1
+      }
   end
 
   def tally(game) do
