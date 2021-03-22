8bd189a7dad9642f93f98fa5e66bf2568ac2ee67
Cornelia Kelinske <corneliakelinske@gmail.com>
2021-03-22 12:11:37 -0700

    updated tests for checking for game won or lost

--- hangman/test/game_test.exs
+++ hangman/test/game_test.exs
@@ -70,28 +70,67 @@ defmodule GameTest do
     assert game.turns_left == 6
   end
 
-  test "lost game is recognized" do
-    game = Game.new_game("w")
-    {game, _tally} = Game.make_move(game, "a")
-    assert game.game_state == :bad_guess
-    assert game.turns_left == 6
-    {game, _tally} = Game.make_move(game, "b")
-    assert game.game_state == :bad_guess
-    assert game.turns_left == 5
-    {game, _tally} = Game.make_move(game, "c")
-    assert game.game_state == :bad_guess
-    assert game.turns_left == 4
-    {game, _tally} = Game.make_move(game, "d")
-    assert game.game_state == :bad_guess
-    assert game.turns_left == 3
-    {game, _tally} = Game.make_move(game, "e")
-    assert game.game_state == :bad_guess
-    assert game.turns_left == 2
-    {game, _tally} = Game.make_move(game, "f")
-    assert game.game_state == :bad_guess
-    assert game.turns_left == 1
-    {game, _tally} = Game.make_move(game, "g")
-    assert game.game_state == :lost
+  # test "lost game is recognized" do
+  #   game = Game.new_game("w")
+  #   {game, _tally} = Game.make_move(game, "a")
+  #   assert game.game_state == :bad_guess
+  #   assert game.turns_left == 6
+  #   {game, _tally} = Game.make_move(game, "b")
+  #   assert game.game_state == :bad_guess
+  #   assert game.turns_left == 5
+  #   {game, _tally} = Game.make_move(game, "c")
+  #   assert game.game_state == :bad_guess
+  #   assert game.turns_left == 4
+  #   {game, _tally} = Game.make_move(game, "d")
+  #   assert game.game_state == :bad_guess
+  #   assert game.turns_left == 3
+  #   {game, _tally} = Game.make_move(game, "e")
+  #   assert game.game_state == :bad_guess
+  #   assert game.turns_left == 2
+  #   {game, _tally} = Game.make_move(game, "f")
+  #   assert game.game_state == :bad_guess
+  #   assert game.turns_left == 1
+  #   {game, _tally} = Game.make_move(game, "g")
+  #   assert game.game_state == :lost
 
+  #end
+
+  test "a won game is recognized" do
+    moves = [
+      {"w", :good_guess},
+      {"i", :good_guess},
+      {"b", :good_guess},
+      {"l", :good_guess},
+      {"e", :won}
+    ]
+
+    game = Game.new_game("wibble")
+
+
+    Enum.reduce(moves,game, fn ({guess, state}, new_game)->
+      { new_game, _} = Game.make_move(new_game, guess)
+      assert new_game.game_state == state
+      new_game
+    end)
+  end
+
+  test "a lost game is recognized" do
+    moves = [
+      {"a", :bad_guess},
+      {"b", :bad_guess},
+      {"c", :bad_guess},
+      {"d", :bad_guess},
+      {"e", :bad_guess},
+      {"f", :bad_guess},
+      {"g", :lost},
+    ]
+
+    game = Game.new_game("w")
+
+    Enum.reduce(moves, game, fn ({guess, state}, new_game)->
+      { new_game, _} = Game.make_move(new_game, guess)
+      assert new_game.game_state == state
+      new_game
+    end)
   end
 end
