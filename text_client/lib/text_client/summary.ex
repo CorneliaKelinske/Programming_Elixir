10f8e81060cc7fb5fb26e98e21f61964441afb6b
Cornelia Kelinske <corneliakelinske@gmail.com>
2021-05-27 07:05:18 -0700

    letters that were already guessed are now displayed in the game

--- text_client/lib/text_client/summary.ex
+++ text_client/lib/text_client/summary.ex
@@ -5,6 +5,7 @@ defmodule TextClient.Summary do
       "\n",
       "Word so far: #{Enum.join(tally.letters, " ")}\n",
       "Guesses left: #{tally.turns_left}\n",
+      "Already guessed: #{tally.used}"
 
     ]
     game
