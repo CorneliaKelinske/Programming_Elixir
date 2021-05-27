ba4093f1c91ab2c07de916b33b3dacaae5cda1aa
Cornelia Kelinske <corneliakelinske@gmail.com>
2021-05-27 06:14:04 -0700

    completed the Prompter module; fixed inout for the accept_move function (changing the return of Summary.display

--- text_client/lib/text_client/prompter.ex
+++ text_client/lib/text_client/prompter.ex
@@ -1,6 +1,8 @@
 defmodule TextClient.Prompter do
 
-  def accept_move(game) do
+  alias TextClient.State
+
+  def accept_move(game = %State{}) do
     IO.gets("Your guess: ")
     |> check_input(game)
 
@@ -16,7 +18,7 @@ defmodule TextClient.Prompter do
     exit(:normal)
   end
 
-  defp check_input(input, game) do
+  defp check_input(input, game = %State{}) do
     input = String.trim(input)
     cond do
       input =~ ~r/\A[a-z]\z/ ->
