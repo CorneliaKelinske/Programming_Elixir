e9a279b6f348555986a5329a2eef2cc43ab71a31
Cornelia Kelinske <corneliakelinske@gmail.com>
2021-05-29 10:07:13 -0700

    made the changes accoridng to the course, but now text_client is not working

--- hangman/lib/hangman/game.ex
+++ hangman/lib/hangman/game.ex
@@ -33,8 +33,9 @@ defmodule Hangman.Game do
     %{
       game_state: game.game_state,
       turns_left: game.turns_left,
-      letters: game.letters |> reveal_guessed(game.used),
-      used: MapSet.to_list(game.used)
+      used: game.used |> MapSet.to_list |> Enum.sort,
+      letters: game.letters |> reveal_guessed(game.used)
+
     }
   end
 
