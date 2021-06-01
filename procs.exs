db84ce696876ae4fab9004011b94a0c980ab5ec4
Cornelia Kelinske <corneliakelinske@gmail.com>
2021-06-01 16:02:54 -0700

    added a reset option to my greeter function

--- procs.exs
+++ procs.exs
@@ -2,8 +2,10 @@ defmodule Procs do
 
  def greeter(count) do
   receive do
-      { :add, n } ->
-        greeter(count+n)
+    { :add, n } ->
+      greeter(count+n)
+    { :reset } ->
+      greeter(0)
     msg ->
       IO.puts "#{count}: Hello #{IO.inspect(msg)}"
       greeter(count)
